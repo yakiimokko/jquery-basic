733946d19204ef56f77e21a83627309d3bb0ad9f
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-21 17:23:05 +0000

    css-operation.js

---
+++ js/css-operation.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('#red').on('click',function(){
+        $('#target').css('color','red');
+    });
+
+    $('#blue').on('click',function(){
+        $('#target').css('color','blue');
+    });
+});
