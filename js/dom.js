07304601db0e34a37e22e6b9763c6dafd4c1ff91
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-21 20:03:32 +0000

    dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,15 +1,9 @@
  $(function() {
-     let num = 0;
-     $('#append').on('click',function(){
-         num++;
-         $('ul').append('<li>appendで追加' + num + '</li>');
-     });
-     
-     $('#remove').on('click',function(){
-         num--;
-         $('li:last').remove();
-         if (num < 0){
-             num = 0;
-         }
-     });
+     $('#show').on('click',function(){
+     $('ul').show();
+    });
+    
+    $('#hide').on('click',function(){
+     $('ul').hide();
+    });
  });
